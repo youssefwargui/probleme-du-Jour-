5a9a173cd966ec79482993167215e0dc80a39d81
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-03-23 18:12:55 +0100

    initial commit

---
+++ probleme18.js
@@ -0,0 +1,11 @@
+/**
+ * Trouve le premier élément qui apparaît un nombre pair de fois dans un tableau.
+ * N'oublie pas de gérer les éléments avec plusieurs occurrences paires et retourne le premier.
+ * Retourne null s'il n'y a aucun élément avec un nombre pair d'occurrences.
+ * Exemple d'utilisation :
+ * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+ * console.log(onlyEven); // 4
+ */
+var evenOccurrence = function(arr) {
+  // Votre code ici.
+};
